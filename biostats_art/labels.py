90a83bd095235c410530d874f5808f36c7a96447
Marlon <marlonvcendron@gmail.com>
2025-01-08 20:19:13 -0300

    reorganização

--- biostats_art/labels.py
+++
@@ -1,6 +0,0 @@
-labels = {
-  'out_sim': 'Similaridade de saída',
-  'in_sim': 'Similaridade de entrada',
-  'model': 'Modelo',
-  'area': 'Área'
-}
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-DATA_PATH = 'dados/dados.csv'
+humano = 'marlon'
+
+DATA_PATH = 'dados/dados.csv' if humano == 'marlon' else r'C:\Users\pcost\Desktop\biostat\dados.csv'
+
 
 def load_data(filter_outliers=True, path=DATA_PATH):
   df = pd.read_csv(path)
@@ -17,3 +20,11 @@ def load_data(filter_outliers=True, path=DATA_PATH):
 
 
   return df
+
+if __name__ == '__main__': 
+  df = load_data()
+  stats = df.groupby(['area', 'model'])['out_sim'].agg(['mean', 'std', 'min', 'max', 'median', 'count'])
+  stats = stats.reset_index()
+  output = pd.DataFrame (stats)
+  output.to_csv(r'C:\Users\pcost\Desktop\biostat\descritivas.csv', index=False)
+
