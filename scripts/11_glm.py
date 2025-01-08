b9a5470856034b0a429dc93b2237ca9b76b1f220
Marlon <marlonvcendron@gmail.com>
2025-01-08 12:07:11 -0300

    glm

--- scripts/11_glm.py
+++ scripts/11_glm.py
@@ -2,9 +2,13 @@ from load_data import load_data
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
+
 if __name__ == '__main__':
   df = load_data()
   formula = 'out_sim ~ model + area + model:area'
+  # formula = 'out_sim ~ model * area * in_sim'
+  # formula = 'out_sim ~ model * area'
+  # formula = 'out_sim ~ model * area + model:in_sim'
   glm = smf.glm(formula=formula, data=df, family=sm.families.Gaussian()).fit()
   print(glm.summary())
 
