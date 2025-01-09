097e0ac930664fd306291420b9aeb3f996a198d6
Marlon <marlonvcendron@gmail.com>
2025-01-09 10:14:59 -0300

    glmm

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -9,14 +9,16 @@ def load_data(filter_outliers=True, path=DATA_PATH):
   df = pd.read_csv(path)
   df['out_sim'] = df['out_sim'].astype(float)
   # df['in_sim'] = df['in_sim'].astype(float)
-  df['in_sim'] = df['in_sim'].astype('category')
+  df['in_sim'] = df['in_sim'].astype(float)
   df['model'] = df['model'].astype('category')
   df['area'] = df['area'].astype('category')
+  df['trial'] = df['trial'].astype('category')
 
   # Remove in_sim 0 e 1
   if filter_outliers:
     df = df.loc[~df['in_sim'].isin([0, 1])]
-    df['in_sim'] = df['in_sim'].cat.remove_unused_categories()
+    # df['in_sim'] = df['in_sim'].cat.remove_unused_categories()
+    # df
 
 
   return df
